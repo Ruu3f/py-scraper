3331a053fbc542ebbe05b61c0a00457d849bc84e
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-06-11 06:54:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from colorama import init, Fore
 
 init()
 
-websites = [url.strip() for url in input("Websites to scrape (use ',' for multiple websites): ").split(',')]
+websites = [
+    url.strip()
+    for url in input("Websites to scrape (use ',' for multiple websites): ").split(",")
+]
 
-subprocess.check_call(['pip', 'install', 'urllib3==1.25.11'])
+subprocess.check_call(["pip", "install", "urllib3==1.25.11"])
+
 
 def scrape_website(url):
     try:
@@ -14,7 +18,7 @@ def scrape_website(url):
         response = scraper.get(url)
 
         if response.status_code == 200:
-            domain = response.url.split('//')[1].split('/')[0]
+            domain = response.url.split("//")[1].split("/")[0]
             folder_path = f"Scraped/{domain}"
             os.makedirs(folder_path, exist_ok=True)
 
@@ -22,8 +26,12 @@ def scrape_website(url):
             with open(index_file_path, "w", encoding="utf-8") as file:
                 file.write(response.text)
 
-            css_urls = re.findall(r'<link.*?href=["\'](.*?)["\']', response.text, flags=re.IGNORECASE)
-            js_urls = re.findall(r'<script.*?src=["\'](.*?)["\']', response.text, flags=re.IGNORECASE)
+            css_urls = re.findall(
+                r'<link.*?href=["\'](.*?)["\']', response.text, flags=re.IGNORECASE
+            )
+            js_urls = re.findall(
+                r'<script.*?src=["\'](.*?)["\']', response.text, flags=re.IGNORECASE
+            )
 
             for css_url in css_urls:
                 absolute_css_url = urljoin(url, css_url)
@@ -38,18 +46,22 @@ def scrape_website(url):
                 js_response = scraper.get(absolute_js_url)
                 js_file_name = os.path.basename(js_url)
                 valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
-                js_file_name = ''.join(c for c in js_file_name if c in valid_chars)
+                js_file_name = "".join(c for c in js_file_name if c in valid_chars)
                 js_file_path = os.path.join(folder_path, js_file_name)
                 with open(js_file_path, "w", encoding="utf-8") as file:
                     file.write(js_response.text)
 
             print(Fore.GREEN + f"Website {url} scraped successfully.")
         else:
-            print(Fore.RED + f"Failed to scrape website {url}. Status code: {response.status_code}")
+            print(
+                Fore.RED
+                + f"Failed to scrape website {url}. Status code: {response.status_code}"
+            )
     except requests.exceptions.RequestException as e:
         print(Fore.RED + f"An error occurred while scraping website {url}: {str(e)}")
     except IOError as e:
         print(Fore.RED + f"Error occurred during file operation: {str(e)}")
+
 
 for website in websites:
     scrape_website(website)
